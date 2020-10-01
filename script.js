8f7c199fd4a239a1102f98bf8556ee25b16dad9a
Muyassr <moyasr.tam@gmail.com>
2020-10-01 09:29:09 +0300

    email check

--- script.js
+++ script.js
@@ -40,6 +40,15 @@ function checkLength(element, min, max) {
     }
 }
 
+function checkEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+      } else {
+        showError(input, 'Email is not valid');
+      }
+}
+
 function checkPasswordsMatch(password, password2) {
     if (password.value===password2.value) {
 
@@ -53,6 +62,7 @@ function checkPasswordsMatch(password, password2) {
         checkRequired([username, email, password, password2]);
         checkLength(username, 3, 15);
         checkLength(password, 6, 25);
+        checkEmail(email);
         checkPasswordsMatch(password, password2);
     });
     
